5b9b4fa8314c800a80c6242d70c24f2e6d4e535f
me2m8 <elmer.brannstrom@gmail.com>
2024-01-13 15:34:32 +0100

    put the gilrs exmple into main

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,15 +1,27 @@
-use serial2::*;
-use robust_arduino_serial::*;
-use std::{time::Duration, alloc::System, process::{exit, self}};
 use gilrs::{Gilrs, Button, Event};
 
 fn main() {
-    let mut gilrs = Gilrs::new().unwrap();   
-    let serial_port = "/dev/cu.usbmodem1101";
-    
-    let mut serial = SerialPort::open(serial_port, 115200);
-        
+    let mut gilrs = Gilrs::new().unwrap();
+
+    // Iterate over all connected gamepads
+    for (_id, gamepad) in gilrs.gamepads() {
+        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
+    }
+
+    let mut active_gamepad = None;
+
     loop {
-        
+        // Examine new events
+        while let Some(Event { id, event, time }) = gilrs.next_event() {
+            println!("{:?} New event from {}: {:?}", time, id, event);
+            active_gamepad = Some(id);
+        }
+
+        // You can also use cached gamepad state
+        if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
+            if gamepad.is_pressed(Button::South) {
+                println!("Button South is pressed (XBox - A, PS - X)");
+            }
+        }
     }
 }
