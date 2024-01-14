f2c49e0ac07d98710a50dd52bba4677be48aa2de
me2m8 <elmer.brannstrom@gmail.com>
2024-01-14 12:17:53 +0100

    Started controller script and moves robot servo logic into its own file

---
+++ controller/src/kinematics.rs
@@ -0,0 +1,20 @@
+use gilrs::{Gilrs, Button, Event};
+use crate::robot::robot::*;
+
+pub struct controller {
+    pub x_axis: f32,
+    pub y_axis: f32,
+    pub a_button: bool,
+    pub b_button: bool,
+    pub x_button: bool,
+    pub y_button: bool,
+    pub left_trigger: f32,
+    pub right_trigger: f32,
+    pub left_bumper: bool,
+    pub right_bumper: bool,
+    pub d_pad_down: bool,
+    pub d_pad_up: bool,
+    pub d_pad_left: bool,
+    pub d_pad_right: bool,
+    pub controll: bool,
+}
--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,100 +1,13 @@
 use std::{io::Read, thread::sleep, time::Duration};
 
-// servo angles
-const MAX_ANGLE: f32 = 180.0;
-const MIN_ANGLE: f32 = 0.0;
+mod kinematics;
+mod robot;
 
-// microseconds for arduino
-const MAX_SERVO: u16 = 2400;
-const MIN_SERVO: u16 = 250;
+use crate::robot::robot::*;
 
 // cummunication
 const PREFIX: &u8 = &b'\r';
 const SUFFIX: &u8 = &b'\n';
-
-mod robot {
-    use crate::{MAX_ANGLE, MAX_SERVO, MIN_ANGLE, MIN_SERVO};
-
-    /// Defines a servo angle, but with more functions on it
-    #[derive(Debug, Copy, Clone)]
-    pub struct Angle(pub f32);
-
-    /// Very specific names for servos
-    #[derive(Debug, Copy, Clone)]
-    pub struct Arm {
-        pub base: Angle,
-        pub shoulder: Angle,
-        pub elbow: Angle,
-        pub claw: Angle,
-    }
-
-    /// angle to servo fucky microseccond
-    impl Into<u16> for Angle {
-        fn into(self) -> u16 {
-            let factor = (self.0 - MIN_ANGLE) / MAX_ANGLE;
-            ((MAX_SERVO - MIN_SERVO) as f32 * factor + MIN_SERVO as f32) as u16
-        }
-    }
-
-    /// he's average alright
-    impl Default for Arm {
-        fn default() -> Self {
-            Self {
-                base: Angle(0.),
-                shoulder: Angle(0.),
-                elbow: Angle(0.),
-                claw: Angle(0.),
-            }
-        }
-    }
-
-    /// Arm functions
-    impl Arm {
-        pub fn to_servos(&self) -> Servos {
-            Servos {
-                base: self.base.into(),
-                shoulder: self.shoulder.into(),
-                elbow: self.elbow.into(),
-                claw: self.claw.into(),
-            }
-        }
-    }
-
-    /// quirky arm
-    #[derive(Debug, Copy, Clone)]
-    pub struct Servos {
-        pub base: u16,
-        pub shoulder: u16,
-        pub elbow: u16,
-        pub claw: u16,
-    }
-
-    impl Servos {
-        pub fn to_message(&self) -> Vec<u8> {
-            unsafe { std::mem::transmute::<Box<Servos>, &[u8; 8]>(Box::new(*self)) }.to_vec()
-        }
-    }
-
-    #[cfg(test)]
-    mod test {
-        use super::*;
-
-        #[test]
-        pub fn servos_to_message() {
-            let servos = Servos {
-                base: 100,
-                shoulder: 200,
-                elbow: 50,
-                claw: 1,
-            };
-
-            let actual = servos.to_message();
-            let expected: Vec<u8> = vec![100, 0, 200, 0, 50, 0, 1, 0];
-
-            assert_eq!(actual, expected);
-        }
-    }
-}
 
 fn handler(buffer: &Vec<u8>) {
     print!("< ");
@@ -110,11 +23,11 @@ fn main() {
         .open()
         .expect("Failed to open port");
 
-    let robot = robot::Arm {
-        base: robot::Angle(0.),
-        shoulder: robot::Angle(80.),
-        elbow: robot::Angle(100.),
-        claw: robot::Angle(110.),
+    let robot = Arm {
+        base: Angle(0.),
+        shoulder: Angle(80.),
+        elbow: Angle(100.),
+        claw: Angle(110.),
     };
 
     sleep(Duration::from_secs(2));
